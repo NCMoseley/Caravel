8ce330651397f2b1cfb55e5303dac7fa9d0fdcd5
NCMoseley <ncmoseley@gmail.com>
2018-06-18 20:26:39 -0700

    Add Styles to carousel, underline hover styles

--- template-parts/carousel-nav.php
+++ template-parts/carousel-nav.php
@@ -10,23 +10,31 @@
   <div class="slider-nav">
 
   <div class="hummingbird">
+    <a href="https://volunteer.hummingbirdnetwork.com" target="_blank">
     <h3>Hummingbird Network</h3>
     <p>The following is placeholder text known as “lorem ipsum,” which is scrambled Latin used by designers to mimic real copy. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque congue porttitor ullamcorper. Nullam sit amet nisi condimentum erat iaculis auctor. Integer tempus, elit in laoreet posuere, lectus neque blandit dui, et placerat urna diam mattis orci.</p>
+     </a>
     <a target="_blank" href="https://volunteer.hummingbirdnetwork.com">View Site</a>
   </div>
   <div class="valo">
+     <a target="_blank" href="https://valosmartcity.com/">
     <h3>Valo Smart Cities</h3>
     <p>The following is placeholder text known as “lorem ipsum,” which is scrambled Latin used by designers to mimic real copy. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque congue porttitor ullamcorper. Nullam sit amet nisi condimentum erat iaculis auctor. Integer tempus, elit in laoreet posuere, lectus neque blandit dui, et placerat urna diam mattis orci.</p>
+    </a>
     <a target="_blank" href="https://valosmartcity.com/">View Site</a>
   </div>
   <div class="pacwest">
+    <a target="_blank" href="https://pacwestcanopy.com/">
     <h3>PacWest Canopy</h3>
     <p>The following is placeholder text known as “lorem ipsum,” which is scrambled Latin used by designers to mimic real copy. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque congue porttitor ullamcorper. Nullam sit amet nisi condimentum erat iaculis auctor. Integer tempus, elit in laoreet posuere, lectus neque blandit dui, et placerat urna diam mattis orci.</p>
+    </a>
     <a target="_blank" href="https://pacwestcanopy.com/">View Site</a>
   </div>
   <div class="nexus">
+    <a  target="_blank" href="https://pacwestcanopy.com/">
     <h3>Nexus</h3>
     <p>The following is placeholder text known as “lorem ipsum,” which is scrambled Latin used by designers to mimic real copy. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque congue porttitor ullamcorper. Nullam sit amet nisi condimentum erat iaculis auctor. Integer tempus, elit in laoreet posuere, lectus neque blandit dui, et placerat urna diam mattis orci.</p>
+    </a>
     <a  target="_blank" href="https://pacwestcanopy.com/">View Site</a>
   </div>
 	
